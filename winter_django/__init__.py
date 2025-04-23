cfd3d4cf7d681f0abea5e47edc5fad40dee6ce14
rushduhhp <rushduhhp@gmail.com>
2025-04-23 10:25:59 -0500

    Update __init__.py

--- winter_django/__init__.py
+++ winter_django/__init__.py
@@ -1,4 +1,4 @@
-from winter.web import arguments_resolver
+76f040b8a26a77cfb0bbbf30ab19427278c9c6bffrom winter.web import arguments_resolver
 from .http_request_argument_resolver import HttpRequestArgumentResolver
 from .output_template import output_template
 from .view import create_django_urls_from_routes
